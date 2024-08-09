97e0ddbf3cc6c7bdcd5932f2fc724ea3728ff228
Finn Brady <finn.t.brady@gmail.com>
2024-08-09 01:53:32 +0100

    Update sketch.js
    Uploaded correct version

--- OtherVisual/sketch.js
+++ OtherVisual/sketch.js
@@ -1,16 +1,16 @@
-let GrassNoiseThreshhold=0.0415;
-let StoneNoiseThreshhold=0.1515;
+let ShallowWaterThreshhold=1.55107;
+let GrassNoiseThreshhold=1.55449;
+let StoneNoiseThreshhold=1.5949;
+let Stone2NoiseThreshhold=1.7102;
 
 let NoiseScale=0.025;
-let a=[];
-let t;
 
 function noiseProcess(x,y){
   //offset to prevent symmetry
-  t=noise(x+123123,y-6969);
+  t=noise(x+123123,y+6969);
   
   //adding octaves
-  t*=2;
+  t*=2.1;
   for(let n=1;n<5;n++){
     t+=pow(t,-n);
   }
@@ -18,13 +18,11 @@ function noiseProcess(x,y){
   //scaling noise down
   t=sqrt(t);
   
-  //subtracting a paraboloid with values clamped so that the island forms an imperfect cirlce
-  t-=constrain((x*x/4+y*y/4-3)/5,-0.015,0.15);
+  //subtracting a paraboloid with values clamped so that the island forms a cirlce, clamping the values so that the island isn't a perfect circle and has variance on the edges
+  t-=constrain(1.6*(x*x/4+y*y/4-3)/5,-0.014,0.15);
   
-  //M A G I C   N U M B E R   (I don't remember what this does)
-  t-=1.5;
-  
-  return t;
+  l=(t>=Stone2NoiseThreshhold)+(t>=StoneNoiseThreshhold)+(t>=GrassNoiseThreshhold)+ (t>=ShallowWaterThreshhold);
+  return [[5,5,225],[90,127,255],[0,255,0],[100,100,10],[200,200,200]][l];
 }
 
 function setup() {
@@ -37,10 +35,9 @@ function draw() {
   translate(width/2,height/2);
   for(let n=-width/4;n<width/4;n++){
     for(let m=-height/4;m<height/4;m++){
-      let a=noiseProcess(n*NoiseScale,m*NoiseScale);
-      if(a>0.5){stroke(255*a,0,0);}
-      else{stroke(0,255*a*2,255*a*2);}
-      point(n,m);
+      a=noiseProcess(n*NoiseScale,m*NoiseScale);
+      stroke(a[0],a[1],a[2]);
+      point(2*n,2*m);
     }
   }
   noLoop();
